ab17d0b7789d359269de36b646c2f5646e2c65b5
Brendon Rofe <b1@xgmi.com>
2023-03-14 11:40:38 +0200

    Mongoose setup

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Brendon:pass123@cluster0.zvg9ytk.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello' });
+app.use(bodyParser.json);
+
+app.get('/todos', (req, res) => {
+  res.json({ todos: 'Todos go here' });
 });
 
 app.listen(3000, () => {
