655706e273fd8501679fc4ffead50dafd563cc5c
Brendon <b1@xgmi.com>
2023-04-25 17:32:20 +0200

    Establish database connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+import mongoose from "mongoose";
+
+const dbUrl = process.env.MONDGODB_URL;
+
+mongoose.connect(`${dbUrl}`);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Connected to database'));
 
 const app = express();
 
